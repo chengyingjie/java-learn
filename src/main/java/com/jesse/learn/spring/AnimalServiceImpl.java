0d79ab8573e4aeba1f02704f108a3b12bda59ce4
chengyingjie <chengyingjie@youzan.com>
2020-11-07 22:02:29 +0800

    雪花算法

--- src/main/java/com/jesse/learn/spring/AnimalServiceImpl.java
+++ src/main/java/com/jesse/learn/spring/AnimalServiceImpl.java
@@ -1,9 +1,15 @@
 package com.jesse.learn.spring;
 
 
+import java.util.UUID;
+
 public class AnimalServiceImpl implements AnimalService {
 
     public void eat() {
         System.out.println("I want to eat...");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID());
     }
 }
